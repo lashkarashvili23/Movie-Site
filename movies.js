83352525de9ade3da00b2f37c5885553e9978931
lashkarashvili23 <lashkarashvili23@gmail.com>
2024-04-16 14:21:33 +0400

    update the player and movies page

--- movies.js
+++ movies.js
@@ -1,17 +1,17 @@
-export const movies = {
-  "47 RONIN":
+export const moviess = {
+  "47 Ronin":
     "https://www.youtube.com/embed/j8cKdDkkIYY?si=bD1D_gkWeamuDPU8&autoplay=1",
-  "SOURCE CODE":
+  "Source Code":
     "https://www.youtube.com/embed/t5roJgHV_lA?si=M6VacGojYEyUC1El&autoplay=1",
-  ARCHIVE:
+  Archive:
     "https://www.youtube.com/embed/m0J0BwIzURI?si=KpX8JA2lehaRI5QN&autoplay=1",
-  "THE MATRIX":
+  "The Matrix":
     "https://www.youtube.com/embed/hMbexEPAOQI?si=NOgWs9ZlGd3YsQWt&autoplay=1",
-  "BLACK MIRROR":
+  "Black Mirror":
     "https://www.youtube.com/embed/5jY1ecibLYo?si=3wrCZRvid6u2u4YF&autoplay=1",
-  DARK: "https://www.youtube.com/embed/ESEUoa-mz2c?si=N1jycfwReSX9o8mt&autoplay=1",
-  HOUSE:
+  Dark: "https://www.youtube.com/embed/ESEUoa-mz2c?si=N1jycfwReSX9o8mt&autoplay=1",
+  House:
     "https://www.youtube.com/embed/GSgua-jsn-Q?si=GghfISEYH6OMFaQW&autoplay=1",
-  "BREAKING BAD":
+  "Breaking Bad":
     "https://www.youtube.com/embed/XZ8daibM3AE?si=9TvF-yIqA7XVDe-u&autoplay=1",
 };
--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 "use strict";
-import { movies } from "./movies.js";
+import { moviess } from "./movies.js";
+import { movies } from "./moviesNew.js";
 
 const mainBtn = document.querySelector(".play");
 // Select all elements with the class name "playNew"
-var playButtons = document.querySelectorAll(".playNew");
+const playButtons = document.querySelectorAll(".playNew");
+// player close button
+const closeBtn = document.querySelector(".closePlayer");
+// 'More' Movies button
+const allMovies = document.querySelector(".moreMovies");
 
 const headline = document.getElementById("headline");
 const sinema = document.createElement("div");
 
-const closeMovie = function () {};
+const closeMovie = function () {
+  parentElement.removeChild(sinema);
+  document.getElementById("homepage").style.display = "none";
+  // headline.style.display = "flex";
+};
 
 const playMovie = function (movieName) {
   //   event.preventDefault();
   headline.style.display = "none";
+  document.getElementById("homepage").style.display = "flex";
   sinema.innerHTML = `
-  <iframe width="100%" height="100%" src="${movies[movieName]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <iframe width="100%" height="100%" src="${moviess[movieName]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <button class='closePlayer'>Close Player</button>
   `;
   sinema.classList.add("sinema_style");
   const homepage = document.getElementById("homepage");
@@ -28,15 +39,61 @@ playButtons.forEach(function (playButton) {
     let titleElement = event.target
       .closest(".movie_card")
       .querySelector(".m_title");
-
     // Get the content of the span element inside "m_title"
     let titleContent = titleElement.textContent;
-
-    // Output the title content to the console (you can do any further processing here)
-    console.log(titleContent);
+    // Output the title content to the console
     playMovie(titleContent);
   });
 });
 
-mainBtn.addEventListener("click", playMovie);
 // mainBtnNew.addEventListener("click", playMovie);
+// mainBtn.addEventListener("click", playMovie);
+
+// Select the parent element that will contain the button(s)
+const parentElement = document.getElementById("homepage");
+
+// Add an event listener to the parent element
+parentElement.addEventListener("click", function (event) {
+  // Check if the clicked element is the button you're interested in
+  if (event.target.matches(".closePlayer")) {
+    // Your event handling logic here
+    closeMovie();
+  }
+});
+
+const moviePageNew = function () {
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("movies").style.paddingTop = "110px";
+  const container = document.querySelector(".container");
+  movies.forEach(function (movie) {
+    const movieCard = document.createElement("div");
+    movieCard.innerHTML = `
+    <div>
+    <a href="#" class="playNew"
+    ><img class="card_img" src="img/test.png" alt=""
+    /></a>
+    </div>
+    <div class="title_date">
+    <div class="m_title"><span>${movie.name}</span></div>
+    <div class="m_rel_date"><span>${movie.year}</span></div>
+    </div>
+    <div class="info_rating">
+    <div class="m_info"><span>HD ${movie.length} min</span></div>
+    <div class="m_rating"><span>${movie.rating}</span></div>
+    </div>
+    `;
+    container.style.flexWrap = "wrap";
+    movieCard.classList.add("movie_card");
+    container.appendChild(movieCard);
+    const moviesNew = document.getElementById("movies");
+    moviesNew.style.overflow = "hidden";
+    moviesNew.classList.remove("height");
+  });
+  // Get a reference to the child element I want to remove
+  const childToRemove = document.getElementById("movieMore");
+
+  // Remove the child element and its children from the parent element
+  container.removeChild(childToRemove);
+};
+
+allMovies.addEventListener("click", moviePageNew);
