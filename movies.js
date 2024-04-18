91243905719b25ee5b28bf6106e591b4c72e5d0e
lashkarashvili23 <lashkarashvili23@gmail.com>
2024-04-18 13:05:04 +0400

    updated "Movies" and "TV Shows" pages

--- movies.js
+++ movies.js
@@ -1,17 +1,73 @@
 export const moviess = {
-  "47 Ronin":
-    "https://www.youtube.com/embed/j8cKdDkkIYY?si=bD1D_gkWeamuDPU8&autoplay=1",
+  "47 Ronin": "https://www.youtube.com/embed/DEoDulGTElE?si=KNsbytk47ApiB7-9",
   "Source Code":
-    "https://www.youtube.com/embed/t5roJgHV_lA?si=M6VacGojYEyUC1El&autoplay=1",
-  Archive:
-    "https://www.youtube.com/embed/m0J0BwIzURI?si=KpX8JA2lehaRI5QN&autoplay=1",
-  "The Matrix":
-    "https://www.youtube.com/embed/hMbexEPAOQI?si=NOgWs9ZlGd3YsQWt&autoplay=1",
+    "https://www.youtube.com/embed/t5roJgHV_lA?si=M6VacGojYEyUC1El",
+  Archive: "https://www.youtube.com/embed/m0J0BwIzURI?si=KpX8JA2lehaRI5QN",
+  "The Matrix": "https://www.youtube.com/embed/vKQi3bBA1y8?si=cj_XRTPnM_bZdZnq",
   "Black Mirror":
-    "https://www.youtube.com/embed/5jY1ecibLYo?si=3wrCZRvid6u2u4YF&autoplay=1",
-  Dark: "https://www.youtube.com/embed/ESEUoa-mz2c?si=N1jycfwReSX9o8mt&autoplay=1",
-  House:
-    "https://www.youtube.com/embed/GSgua-jsn-Q?si=GghfISEYH6OMFaQW&autoplay=1",
+    "https://www.youtube.com/embed/5jY1ecibLYo?si=3wrCZRvid6u2u4YF",
+  Dark: "https://www.youtube.com/embed/ESEUoa-mz2c?si=N1jycfwReSX9o8mt",
+  House: "https://www.youtube.com/embed/GSgua-jsn-Q?si=GghfISEYH6OMFaQW",
   "Breaking Bad":
-    "https://www.youtube.com/embed/XZ8daibM3AE?si=9TvF-yIqA7XVDe-u&autoplay=1",
+    "https://www.youtube.com/embed/XZ8daibM3AE?si=9TvF-yIqA7XVDe-u",
+  Interstellar: "https://www.youtube.com/embed/zSWdZVtXT7E?si=Ye10NtrHREn1nHf9",
+  Inception: "https://www.youtube.com/embed/YoHD9XEInc0?si=y492c1y2pPETTgCu",
+  "Seven Pounds":
+    "https://www.youtube.com/embed/hvu2F6t26hs?si=SFzcrO1ZvpyQF4pa",
+  Snatch: "https://www.youtube.com/embed/9Jar2XkBboo?si=L4grck9RyWKTkV6q",
+  "Sherlock Holmes":
+    "https://www.youtube.com/embed/iKUzhzustok?si=lbHNCQHxfYWuMOOH",
+  "Fight Club": "https://www.youtube.com/embed/P2MFvE8dpmo?si=JjrHAPJPAkpeiq1m",
+  Crash: "https://www.youtube.com/embed/dgepOrbliX4?si=eBIr39YyyafI6adJ",
+  "The Last Samurai":
+    "https://www.youtube.com/embed/bER_Oeeon9k?si=BEP0EFv-1o0Scrx5",
+  "American History X":
+    "https://www.youtube.com/embed/XfQYHqsiN5g?si=h9HLQEY0WYm3UnfU",
+  "The Usual Suspects":
+    "https://www.youtube.com/embed/Q0eCiCinc4E?si=Snh4p7zcIaFAXeiu",
+  300: "https://www.youtube.com/embed/UrIbxk7idYA?si=QkprX-Grg_XFNhyh",
+  Dogville: "https://www.youtube.com/embed/dC3UFn5MeCg?si=w1eKjOmZiRulp24v",
+  "Who Am I": "https://www.youtube.com/embed/5vnjheCqRIs?si=PP-f08V13_xbQrq1",
+  Rush: "https://www.youtube.com/embed/N1_tE1vaoiY?si=tC955CidY_W5-wQu",
+  "The Other Boleyn Girl":
+    "https://www.youtube.com/embed/M44ZHmPoLSU?si=Qro0CBkose-EnGKt",
+  "Law Abiding Citizen":
+    "https://www.youtube.com/embed/_XRNTj5uhag?si=0F709BaZ3-ZiA1qo",
+  "Seven Psychopaths":
+    "https://www.youtube.com/embed/11WPJEm6ZVA?si=RbW2JNG_izZ2q45A",
+  Disconnect: "https://www.youtube.com/embed/jjiHpi4epQA?si=wODtRdTZgPczgUCa",
+  Fearless: "https://www.youtube.com/embed/W8pfFuBlwKg?si=bz0kDjk7JyMRamTm",
+  Wanted: "https://www.youtube.com/embed/edpEspHOeVU?si=xfnnezzS1ESTbjJj",
+  "Game of Thrones":
+    "https://www.youtube.com/embed/KPLWWIOCOOQ?si=z6KtNhsyvczeFvub",
+  "The Last of Us":
+    "https://www.youtube.com/embed/uLtkt8BonwM?si=7pY5_CFBpFJBTAci",
+  "Stranger Things":
+    "https://www.youtube.com/embed/b9EkMc79ZSU?si=x-PB2N0WWjzPAbeI",
+  Euphoria: "https://www.youtube.com/embed/UR4Pxgnm-GA?si=BibQd2YFAFO7yBBa",
+  "True Detective":
+    "https://www.youtube.com/embed/fVQUcaO4AvE?si=79dFMwQaM8FiJ-M5",
+  "Love, Death & Robots":
+    "https://www.youtube.com/embed/wUFwunMKa4E?si=5XidrQExC7Md8aMD",
+  "The Queen's Gambit":
+    "https://www.youtube.com/embed/oZn3qSgmLqI?si=-E6jSi5WNFdJ79Ao",
+  "House of the Dragon":
+    "https://www.youtube.com/embed/DotnJ7tTA34?si=DfHzuf8aEcb4I14h",
+  Supernatural: "https://www.youtube.com/embed/wCyw3mlvWUo?si=An19Zl3uRYjW7WNm",
+  "The Witcher":
+    "https://www.youtube.com/embed/ndl1W4ltcmg?si=DqlJP1DUj-cUimqR",
+  "Altered Carbon":
+    "https://www.youtube.com/embed/M8PsZki6NGU?si=d-3jZdXvIzBvgsiL",
+  "The New Pope":
+    "https://www.youtube.com/embed/sIoK5D3Bums?si=hzI10jCjwjY3YoU2",
+  "The 100": "https://www.youtube.com/embed/ia1Fbg96vL0?si=gF6K74lhtGYf-v57",
+  Chernobyl: "https://www.youtube.com/embed/s9APLXM9Ei8?si=O1EPA7NKBs1NIizR",
+  "Money Heist":
+    "https://www.youtube.com/embed/_InqQJRqGW4?si=-yPaUWEh8ZN2cerv",
+  "The Young Pope":
+    "https://www.youtube.com/embed/N2ZFdepTu-w?si=oxNwvWQ83fkY7CBH",
+  Narcos: "https://www.youtube.com/embed/xl8zdCY-abw?si=5CHcpVHFoamsiQMJ",
+  Sherlock: "https://www.youtube.com/embed/iK7oLSM7g8w?si=_H0pTslyqzAfuR1-",
+  Vikings: "https://www.youtube.com/embed/9GgxinPwAGc?si=mmX3RZZ4OQYrtEKF",
+  "The Boys": "https://www.youtube.com/embed/M1bhOaLV4FU?si=oJ_scRjx9w4dvtmU",
 };
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 "use strict";
 import { moviess } from "./movies.js";
-import { movies } from "./moviesNew.js";
+import { movies, tvShows } from "./moviesNew.js";
 
+// const mainBtn = document.querySelector(".play");
 const mainBtn = document.querySelector(".play");
 // Select all elements with the class name "playNew"
-const playButtons = document.querySelectorAll(".playNew");
+let playButtons = document.querySelectorAll(".playNew");
 // player close button
 const closeBtn = document.querySelector(".closePlayer");
 // 'More' Movies button
 const allMovies = document.querySelector(".moreMovies");
+const allTvShows = document.querySelector(".moreTvShows");
 
 const headline = document.getElementById("headline");
 const sinema = document.createElement("div");
@@ -24,7 +26,7 @@ const playMovie = function (movieName) {
   headline.style.display = "none";
   document.getElementById("homepage").style.display = "flex";
   sinema.innerHTML = `
-  <iframe width="100%" height="100%" src="${moviess[movieName]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <iframe width="100%" height="100%" src="${moviess[movieName]}&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   <button class='closePlayer'>Close Player</button>
   `;
   sinema.classList.add("sinema_style");
@@ -35,10 +37,17 @@ const playMovie = function (movieName) {
 // Add click event listener to each play button
 playButtons.forEach(function (playButton) {
   playButton.addEventListener("click", function (event) {
-    // Get the nearest ancestor element with the class name "m_title"
-    let titleElement = event.target
-      .closest(".movie_card")
-      .querySelector(".m_title");
+    // Get the nearest ancestor element with the class name "movie_card" or "show_card"
+    let cardElement = event.target.closest(".movie_card, .show_card");
+    if (!cardElement) return; // Exit the function if the card element is not found
+    // Get the title content based on the card type
+    let titleElement;
+    if (cardElement.classList.contains("movie_card")) {
+      titleElement = cardElement.querySelector(".m_title");
+    } else if (cardElement.classList.contains("show_card")) {
+      titleElement = cardElement.querySelector(".m_title");
+      // Adjust this if the class or structure is different for show cards
+    }
     // Get the content of the span element inside "m_title"
     let titleContent = titleElement.textContent;
     // Output the title content to the console
@@ -64,13 +73,12 @@ parentElement.addEventListener("click", function (event) {
 const moviePageNew = function () {
   document.getElementById("homepage").style.display = "none";
   document.getElementById("movies").style.paddingTop = "110px";
-  const container = document.querySelector(".container");
+  const containerM = document.querySelector(".containerM");
   movies.forEach(function (movie) {
     const movieCard = document.createElement("div");
     movieCard.innerHTML = `
-    <div>
     <a href="#" class="playNew"
-    ><img class="card_img" src="img/test.png" alt=""
+    ><img class="card_img" src="img/movPosters/${movie.imgSrc}" alt=""
     /></a>
     </div>
     <div class="title_date">
@@ -80,20 +88,83 @@ const moviePageNew = function () {
     <div class="info_rating">
     <div class="m_info"><span>HD ${movie.length} min</span></div>
     <div class="m_rating"><span>${movie.rating}</span></div>
-    </div>
     `;
-    container.style.flexWrap = "wrap";
+    containerM.style.flexWrap = "wrap";
     movieCard.classList.add("movie_card");
-    container.appendChild(movieCard);
+    containerM.appendChild(movieCard);
     const moviesNew = document.getElementById("movies");
     moviesNew.style.overflow = "hidden";
-    moviesNew.classList.remove("height");
+    moviesNew.classList.remove("heightM");
   });
   // Get a reference to the child element I want to remove
-  const childToRemove = document.getElementById("movieMore");
+  const childToRemoveM = document.getElementById("movieMore");
 
   // Remove the child element and its children from the parent element
-  container.removeChild(childToRemove);
+  containerM.removeChild(childToRemoveM);
+  newEventListener();
 };
 
 allMovies.addEventListener("click", moviePageNew);
+
+const tvShowPageNew = function () {
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("movies").style.display = "none";
+  document.getElementById("tv_shows").style.paddingTop = "110px";
+  const containerS = document.querySelector(".containerS");
+  tvShows.forEach(function (tvShow) {
+    const showCard = document.createElement("div");
+    showCard.innerHTML = `
+    <div>
+    <a href="#" class="playNew"
+    ><img class="card_img" src="img/movPosters/${tvShow.imgSrc}" alt=""
+    /></a>
+    </div>
+    <div class="title_date">
+    <div class="m_title"><span>${tvShow.name}</span></div>
+    <div class="m_rel_date"><span>${tvShow.year}</span></div>
+    </div>
+    <div class="info_rating">
+    <div class="m_info"><span>HD ${tvShow.length} min</span></div>
+    <div class="m_rating"><span>${tvShow.rating}</span></div>
+    </div>
+    `;
+    containerS.style.flexWrap = "wrap";
+    showCard.classList.add("show_card");
+    containerS.appendChild(showCard); //
+    const tvShowsNew = document.getElementById("tv_shows");
+    tvShowsNew.style.overflow = "hidden";
+    tvShowsNew.classList.remove("heightS");
+  });
+  // Get a reference to the child element I want to remove
+  const childToRemoveS = document.getElementById("tvShowMore");
+
+  // Remove the child element and its children from the parent element
+  containerS.removeChild(childToRemoveS);
+  newEventListener();
+};
+
+allTvShows.addEventListener("click", tvShowPageNew);
+
+// Adds an event listener to the recreated elements
+const newEventListener = function () {
+  const playButtonsNew = document.querySelectorAll(".playNew");
+  playButtonsNew.forEach(function (playButton) {
+    playButton.addEventListener("click", function (event) {
+      // Get the nearest ancestor element with the class name "movie_card" or "show_card"
+      let cardElement = event.target.closest(".movie_card, .show_card");
+      if (!cardElement) return; // Exit the function if the card element is not found
+      // Get the title content based on the card type
+      let titleElement;
+      if (cardElement.classList.contains("movie_card")) {
+        titleElement = cardElement.querySelector(".m_title");
+      } else if (cardElement.classList.contains("show_card")) {
+        titleElement = cardElement.querySelector(".m_title");
+        // Adjust this if the class or structure is different for show cards
+      }
+      // Get the content of the span element inside "m_title"
+      let titleContent = titleElement.textContent;
+      // Output the title content to the console
+      playMovie(titleContent);
+    });
+  });
+};
