1cda927adf65caa6df924f3ab06e0a3d8837c19a
lashkarashvili23 <lashkarashvili23@gmail.com>
2024-04-19 12:08:42 +0400

    aded search function

--- script.js
+++ script.js
@@ -2,15 +2,24 @@
 import { moviess } from "./movies.js";
 import { movies, tvShows } from "./moviesNew.js";
 
+const reloadBtn = document.querySelector(".pageReload");
+let movieOn = true;
+let tvShowOn = true;
+let moviesOpened = false;
+let tvShowOpend = false;
+
 // const mainBtn = document.querySelector(".play");
 const mainBtn = document.querySelector(".play");
+const searchBtn = document.getElementById("search_Btn");
 // Select all elements with the class name "playNew"
 let playButtons = document.querySelectorAll(".playNew");
 // player close button
 const closeBtn = document.querySelector(".closePlayer");
 // 'More' Movies button
 const allMovies = document.querySelector(".moreMovies");
+const allMoviesNav = document.querySelector(".moreMoviesNav");
 const allTvShows = document.querySelector(".moreTvShows");
+const allTvShowsNav = document.querySelector(".moreTvShowsNav");
 
 const headline = document.getElementById("headline");
 const sinema = document.createElement("div");
@@ -71,12 +80,16 @@ parentElement.addEventListener("click", function (event) {
 });
 
 const moviePageNew = function () {
-  document.getElementById("homepage").style.display = "none";
-  document.getElementById("movies").style.paddingTop = "110px";
-  const containerM = document.querySelector(".containerM");
-  movies.forEach(function (movie) {
-    const movieCard = document.createElement("div");
-    movieCard.innerHTML = `
+  if (movieOn) {
+    document.getElementById("homepage").style.display = "none";
+    document.getElementById("tv_shows").style.display = "none";
+    document.getElementById("movies").style.display = "flex"; // ???
+    document.getElementById("movies").style.paddingTop = "110px";
+    const containerM = document.querySelector(".containerM");
+    if (!moviesOpened) {
+      movies.forEach(function (movie) {
+        const movieCard = document.createElement("div");
+        movieCard.innerHTML = `
     <a href="#" class="playNew"
     ><img class="card_img" src="img/movPosters/${movie.imgSrc}" alt=""
     /></a>
@@ -89,31 +102,44 @@ const moviePageNew = function () {
     <div class="m_info"><span>HD ${movie.length} min</span></div>
     <div class="m_rating"><span>${movie.rating}</span></div>
     `;
-    containerM.style.flexWrap = "wrap";
-    movieCard.classList.add("movie_card");
-    containerM.appendChild(movieCard);
-    const moviesNew = document.getElementById("movies");
-    moviesNew.style.overflow = "hidden";
-    moviesNew.classList.remove("heightM");
-  });
-  // Get a reference to the child element I want to remove
-  const childToRemoveM = document.getElementById("movieMore");
-
-  // Remove the child element and its children from the parent element
-  containerM.removeChild(childToRemoveM);
-  newEventListener();
+        containerM.style.flexWrap = "wrap";
+        movieCard.classList.add("movie_card");
+        containerM.appendChild(movieCard);
+        const moviesNew = document.getElementById("movies");
+        moviesNew.style.overflow = "hidden";
+        moviesNew.classList.remove("heightM");
+      });
+      // Get a reference to the child element I want to remove
+      let childToRemoveM = document.getElementById("movieMore");
+      if (childToRemoveM) {
+        containerM.removeChild(childToRemoveM);
+      }
+      // Remove the child element and its children from the parent element
+      moviesOpened = !moviesOpened;
+    }
+    newEventListener();
+    movieOn = !movieOn;
+    tvShowOn = true;
+  }
+  console.log(movieOn);
+  console.log(tvShowOn);
+  console.log(moviesOpened);
 };
 
 allMovies.addEventListener("click", moviePageNew);
+allMoviesNav.addEventListener("click", moviePageNew);
 
 const tvShowPageNew = function () {
-  document.getElementById("homepage").style.display = "none";
-  document.getElementById("movies").style.display = "none";
-  document.getElementById("tv_shows").style.paddingTop = "110px";
-  const containerS = document.querySelector(".containerS");
-  tvShows.forEach(function (tvShow) {
-    const showCard = document.createElement("div");
-    showCard.innerHTML = `
+  if (tvShowOn) {
+    document.getElementById("tv_shows").style.display = "flex";
+    document.getElementById("homepage").style.display = "none";
+    document.getElementById("movies").style.display = "none";
+    document.getElementById("tv_shows").style.paddingTop = "110px";
+    const containerS = document.querySelector(".containerS");
+    if (!tvShowOpend) {
+      tvShows.forEach(function (tvShow) {
+        const showCard = document.createElement("div");
+        showCard.innerHTML = `
     <div>
     <a href="#" class="playNew"
     ><img class="card_img" src="img/movPosters/${tvShow.imgSrc}" alt=""
@@ -128,22 +154,32 @@ const tvShowPageNew = function () {
     <div class="m_rating"><span>${tvShow.rating}</span></div>
     </div>
     `;
-    containerS.style.flexWrap = "wrap";
-    showCard.classList.add("show_card");
-    containerS.appendChild(showCard); //
-    const tvShowsNew = document.getElementById("tv_shows");
-    tvShowsNew.style.overflow = "hidden";
-    tvShowsNew.classList.remove("heightS");
-  });
-  // Get a reference to the child element I want to remove
-  const childToRemoveS = document.getElementById("tvShowMore");
-
-  // Remove the child element and its children from the parent element
-  containerS.removeChild(childToRemoveS);
-  newEventListener();
+        containerS.style.flexWrap = "wrap";
+        showCard.classList.add("show_card");
+        containerS.appendChild(showCard); //
+        const tvShowsNew = document.getElementById("tv_shows");
+        tvShowsNew.style.overflow = "hidden";
+        tvShowsNew.classList.remove("heightS");
+      });
+      // Get a reference to the child element I want to remove
+      let childToRemoveS = document.getElementById("tvShowMore");
+      if (childToRemoveS) {
+        containerS.removeChild(childToRemoveS);
+      }
+      tvShowOpend = !tvShowOpend;
+    }
+    // Remove the child element and its children from the parent element
+    newEventListener();
+    tvShowOn = !tvShowOn;
+    movieOn = true;
+  }
+  console.log(movieOn);
+  console.log(tvShowOn);
+  console.log(tvShowOpend);
 };
 
 allTvShows.addEventListener("click", tvShowPageNew);
+allTvShowsNav.addEventListener("click", tvShowPageNew);
 
 // Adds an event listener to the recreated elements
 const newEventListener = function () {
@@ -168,3 +204,47 @@ const newEventListener = function () {
     });
   });
 };
+const homeReload = function refreshPage() {
+  window.location.reload();
+  window.scrollTo(0, 0);
+  window.location.href = "";
+};
+reloadBtn.addEventListener("click", homeReload);
+
+// function hasPropertyIgnoreCase(obj, propName) {
+//   const lowerPropName = propName.toLowerCase(); // Convert the property name to lowercase
+//   return Object.keys(obj).some((key) => key.toLowerCase() === lowerPropName); // Check if any key matches the lowercase property name
+// }
+function toTitleCase(str) {
+  return str.toLowerCase().replace(/\b\w/g, function (txt) {
+    return txt.toUpperCase();
+  });
+}
+
+const searchFilm = function (filmName) {
+  if (moviess.hasOwnProperty(filmName)) {
+    playMovie(filmName);
+    window.scrollTo(0, 0);
+    // window.location.href = "";
+  } else {
+    alert(`Movie/TV Show named "${filmName}" doesn't found`);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  let searchInput = document.getElementById("searchInput");
+  let searchBtn = document.getElementById("search_Btn");
+
+  // Add event listener for input event
+  searchInput.addEventListener("input", function () {
+    let searchTerm = searchInput.value.trim().toLowerCase(); // Get the search term
+  });
+
+  // Add event listener for click event on search button
+  searchBtn.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+    let searchTerm = searchInput.value.trim().toLowerCase(); // Get the search term
+    console.log(searchTerm); // Log the search term
+    searchFilm(toTitleCase(searchTerm));
+  });
+});
